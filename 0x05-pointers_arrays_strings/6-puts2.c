3b1c2d00498eb63d7035a962744e5580a4b31f44
Damarice <menganyidamarice@gmail.com>
2022-09-19 10:46:49 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,34 @@
 #include "main.h"
 
 /**
-* puts2 - prints every other character of a  string to stdout
-* @str: string to print
-*
-* Return: void
+* cap_string - a function that capitalizes all words of a string
+* @n: input string
+* Return: caps on first letter of a separator
 */
-void puts2(char *str)
+char *cap_string(char *n)
 {
-	int i = 0;
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t'};
 
-	while (*(str + i))
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - cap;
+		}
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
 	}
-	_putchar('\n');
+	return (n);
 }
