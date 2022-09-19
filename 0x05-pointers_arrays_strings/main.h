7cdb446919ba47f1f730e16797cd2f5648ef3383
Damarice <menganyidamarice@gmail.com>
2022-09-19 05:24:29 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,24 +3,6 @@
 
 #include <stdio.h>
 
-/**
-* main - header file for prototypes
-* void prototypes (void)
-*
-* int prototypes (int)
-*/
-
-void print_number(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_number(int n);
-int _putcher(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
