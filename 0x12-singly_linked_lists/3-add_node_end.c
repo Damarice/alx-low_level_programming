614a6dd6f1d0d6bda61e6e88a64a337e27637810
Damarice <menganyidamarice@gmail.com>
2022-10-24 06:17:20 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -42,7 +42,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		last = *head;
 		while (last->next != NULL)
 			last = last->next;
-		last = last->next;
+		last->next = new;
 	}
 
 	return (*head);
